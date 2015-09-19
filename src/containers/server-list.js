176a8ea53c9b877d33bd52833cc57974101b76ab
Eduardo Nunes <esnunes@gmail.com>
2015-09-18 23:54:28 -0300

    Add servers status messages

--- src/containers/server-list.js
+++ src/containers/server-list.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { loadServerList } from '../actions/servers';
+import { setStatus, clearStatus } from '../actions/status';
 
 
 const style = {
@@ -14,6 +15,7 @@ const style = {
 class ServerList extends Component {
 
   static propTypes = {
+    children: PropTypes.node,
     dispatch: PropTypes.func.isRequired,
     loading: PropTypes.bool.isRequired,
     error: PropTypes.any,
@@ -24,55 +26,67 @@ class ServerList extends Component {
     history: PropTypes.object.isRequired,
   };
 
-  componentDidMount () {
-    const { dispatch } = this.props;
+  componentWillMount () {
+    const { dispatch, loading } = this.props;
+    if (loading) dispatch(setStatus('Loading list of servers...'));
     dispatch(loadServerList());
-    if (this.refs.list) this.refs.list.focus();
+  }
+
+  componentWillReceiveProps ({ error, loading, servers }) {
+    const { dispatch } = this.props;
+    if (error) dispatch(setStatus(error));
+    if (loading) dispatch(setStatus('Loading list of servers...'));
+    if (servers) {
+      const [ server ] = servers;
+      dispatch(setStatus(server
+        ? `${server.client} server at ${server.host}:${server.port}`
+        : 'No servers found, add a new one.'
+      ));
+    }
   }
 
   componentDidUpdate () {
-    if (this.refs.list) this.refs.list.focus();
+    this.refs.list.focus();
+  }
+
+  componentWillUnmount () {
+    this.props.dispatch(clearStatus());
   }
 
   onItemSelected (e, i) {
-    if (!i) return this.context.history.pushState(null, '/server/add');
+    // if (i === 0) return this.context.history.pushState(null, '/server/add');
+  }
+
+  onSelectItem () {
+    const server = this.props.servers[this.refs.list.selected];
+    this.props.dispatch(
+      setStatus(`${server.client} server at ${server.host}:${server.port}`)
+    );
   }
 
   render () {
     const { loading, servers, error } = this.props;
-    if (error) {
-      return (
-        <box
-          left="center" top="center" shrink="true" style={style.error}>
-          failed to retrieve list of servers
-        </box>
-      );
-    }
-    if (loading) {
-      return (
-        <box left="center" top="center" shrink="true">
-          loading list of servers...
-        </box>
-      );
-    }
+    if (error || loading) return <element width={0} />;
 
-    const items = servers.map(c => c.name).sort();
-    items.unshift('Add new server...');
+    const items = servers.map(c => c.name);
     return (
-      <list
-        ref="list"
-        style={style.list}
-        border={{ type: 'line' }}
-        keys="true"
-        mouse="true"
-        label="Server list"
-        scrollbar={{
-          ch: ' ',
-          track: { bg: 'cyan' },
-          style: { inverse: true },
-        }}
-        items={items}
-        onSelect={::this.onItemSelected}
+      <list ref="list"
+            style={style.list}
+            left="center"
+            top="center"
+            border="line"
+            keys="true"
+            mouse="true"
+            shadow="true"
+            label=" Server list "
+            scrollbar={{
+              ch: ' ',
+              track: { bg: 'cyan' },
+              style: { inverse: true },
+            }}
+            items={items}
+            onSelect={::this.onItemSelected}
+            {...{ 'onSelect Item': ::this.onSelectItem } }
       />
     );
   }
