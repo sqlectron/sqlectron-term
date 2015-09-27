37121a3ca385fda1ba2392125adccd330c19f559
Eduardo Nunes <esnunes@gmail.com>
2015-09-27 20:39:02 -0300

    Add support to retrieve an available port

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,6 @@
+import pf from 'portfinder';
+
+
 export function homedir() {
   return process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
 }
@@ -6,5 +9,15 @@ export function homedir() {
 export function wait (time) {
   return new Promise(resolve => {
     setTimeout(() => resolve(), time);
+  });
+}
+
+
+export function getPort () {
+  return new Promise((resolve, reject) => {
+    pf.getPort((err, port) => {
+      if (err) return reject(err);
+      resolve(port);
+    });
   });
 }
