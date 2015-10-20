fb7297a3c95d2ed7178213027334ac23bec4a690
Eduardo Nunes <esnunes@gmail.com>
2015-10-20 20:50:54 -0200

    Improve shortcuts smart component
    Shortcuts are now handled by the Shortcuts component.

--- src/containers/shortcuts.js
+++ src/containers/shortcuts.js
@@ -11,6 +11,8 @@ class Shortcuts extends Component {
 
     children: PropTypes.element.isRequired,
     items: PropTypes.array.isRequired,
+
+    onShortcut: PropTypes.func,
   };
 
   componentWillUnmount () {
@@ -25,10 +27,19 @@ class Shortcuts extends Component {
     this.props.dispatch(removeShortcuts(this.props.items));
   }
 
+  handleKeypress (ch, info) {
+    const shortcut = this.props.items.find(item => item.key === info.full);
+    if (!shortcut) return undefined;
+
+    if (shortcut.handler) return shortcut.handler(shortcut);
+    if (this.props.onShortcut) return this.props.onShortcut(shortcut);
+  }
+
   render () {
     return cloneElement(this.props.children, {
       onFocus: ::this.handleFocus,
       onBlur: ::this.handleBlur,
+      onKeypress: ::this.handleKeypress,
     });
   }
 
