294df0402d8cd054422482f9ca963479bdad137c
Eduardo Nunes <esnunes@gmail.com>
2015-10-25 18:05:03 -0200

    Only remove shortcuts of focused elements

--- src/containers/shortcuts.js
+++ src/containers/shortcuts.js
@@ -16,20 +16,23 @@ class Shortcuts extends Component {
   };
 
   componentWillUnmount () {
-    this.props.dispatch(removeShortcuts(this.props.items));
+    if (this.focused) this.props.dispatch(removeShortcuts(this.props.items));
+    this.focused = false;
   }
 
   handleFocus () {
+    this.focused = true;
     this.props.dispatch(addShortcuts(this.props.items));
   }
 
   handleBlur () {
+    this.focused = false;
     this.props.dispatch(removeShortcuts(this.props.items));
   }
 
   handleKeypress (ch, info) {
     const shortcut = this.props.items.find(item => item.key === info.full);
-    if (!shortcut) return undefined;
+    if (!shortcut) return false;
 
     if (shortcut.handler) return shortcut.handler(shortcut);
     if (this.props.onShortcut) return this.props.onShortcut(shortcut);
