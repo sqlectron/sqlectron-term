0599315130b33adda727f7832ff42f48ea3023a9
Eduardo Nunes <esnunes@gmail.com>
2015-10-27 21:43:01 -0200

    Add style to remove server screen

--- src/containers/server-remove.js
+++ src/containers/server-remove.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import { merge } from 'lodash';
 import { connect } from 'react-redux';
 import { removeServer } from '../actions/servers';
 import { setStatus } from '../actions/status';
@@ -14,6 +15,7 @@ class ServerRemove extends Component {
 
   static contextTypes = {
     history: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired,
   };
 
   componentWillMount () {
@@ -32,27 +34,46 @@ class ServerRemove extends Component {
     this.context.history.goBack();
   }
 
+  handleKeypress (ch, key) {
+    switch (key.full) {
+    case 'tab':
+      this.refs.box.screen.focusNext();
+      break;
+    case 'S-tab':
+      this.refs.box.screen.focusPrev();
+      break;
+    default: return false;
+    }
+  }
+
   render () {
     const { servers, params } = this.props;
+    const { theme } = this.context;
     const server = servers[parseInt(params.id, 10)];
+    const styles = merge({}, theme.box.normal, theme.box.focus, theme.danger);
     return (
-      <box shrink shadow position={{ left: 'center', top: 'center', height: 8 }} border="line">
+      <box shrink shadow ref="box" position={{ left: 'center', top: 'center', height: 8 }} border="line" style={styles}>
         <text
           position={{ top: 1 }}
           height={2}
+          style={styles}
           content={` Are you sure you want to remove server [${server.client} - ${server.name}]? `}
         />
-        <box left="center" position={{ bottom: 1, height: 1, width: 10 }}>
+        <box position={{ left: 'center', bottom: 1, height: 1, width: 10 }} style={styles}>
           <button
-            shrink mouse keys
+            shrink mouse keys input
             position={{ left: 0, bottom: 0 }}
             content=" Yes "
+            style={theme.button}
+            onKeypress={::this.handleKeypress}
             onPress={this.onPress.bind(this, 'yes')}
           />
           <button
-            shrink mouse keys
+            shrink mouse keys focused
             position={{ left: 6, bottom: 0 }}
             content=" No "
+            style={theme.button}
+            onKeypress={::this.handleKeypress}
             onPress={this.onPress.bind(this, 'no')}
           />
         </box>
